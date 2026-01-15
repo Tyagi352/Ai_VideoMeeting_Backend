7f3a25a89526e1f56cb481b1cec8559186b28fdd
Tyagi352 <arnavtyagi116@gmail.com>
2026-01-15 21:31:11 +0530

    index file changed

--- index.js
+++ index.js
@@ -92,6 +92,6 @@ io.on('connection', (socket) => {
   });
 });
 
-// server.listen(PORT, () => {
-//   console.log(`Server running on port ${PORT}`);
-// });
+server.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
